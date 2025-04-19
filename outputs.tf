8e9f906d9a3ee5532c80a4566eb8e0003de3d9b5
John <jhancock1975@gmail.com>
2025-04-19 16:14:17 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "website_url" {
-  description = "Public website endpoint of the static site"
-  value       = data.aws_s3_bucket.static_site.website_endpoint
+  description = "URL of the static website"
+  value       = aws_s3_bucket.website.website_endpoint
 }
