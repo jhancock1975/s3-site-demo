61004017b5d0a3e87e2305003cc94abd7fd138c1
John Hancock <jhancock1975@gmail.com>
2025-04-26 17:52:19 -0400

    use a loggedout page

--- static/logout.js
+++ static/logout.js
@@ -1,14 +1,12 @@
 (function() {
-  // 1) Clear tokens from localStorage
-  localStorage.removeItem("id_token");
-  localStorage.removeItem("access_token");
-  // If you stored a refresh token, clear it too:
-  localStorage.removeItem("refresh_token");
+  //Wipe local tokens
+  ['id_token','access_token','refresh_token'].forEach(k => localStorage.removeItem(k));
+
 
   // 2) Build the Cognito logout URL
   const CLIENT_ID     = "4mt7pq78npi8s55lsetnq1g5c7";
   const COGNITO_DOMAIN= "us-east-13k54foqzh.auth.us-east-1.amazoncognito.com";
-  const REDIRECT_URI  = "https://taptupo.com/index.html";  // where you want users to land after logout
+  const REDIRECT_URI  = "https://taptupo.com/loggedout.html";  // where you want users to land after logout
 
   const logoutUrl = 
     `https://${COGNITO_DOMAIN}/logout?` +
