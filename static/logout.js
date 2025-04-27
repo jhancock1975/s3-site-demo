02de1ffbdb83bdb5f9f47f886958f333b9f016eb
John Hancock <jhancock1975@gmail.com>
2025-04-26 21:01:09 -0400

    add paths

--- static/logout.js
+++ static/logout.js
@@ -1,21 +1,16 @@
 (function() {
-  //Wipe local tokens
+  // Wipe local tokens
   ['id_token','access_token','refresh_token'].forEach(k => localStorage.removeItem(k));
-
-
-  // 2) Build the Cognito logout URL
-  const CLIENT_ID     = "4mt7pq78npi8s55lsetnq1g5c7";
-  const COGNITO_DOMAIN= "us-east-13k54foqzh.auth.us-east-1.amazoncognito.com";
-  const REDIRECT_URI  = "https://taptupo.com/loggedout.html";  // where you want users to land after logout
-
-  const logoutUrl = 
+  // Build the Cognito logout URL
+  const CLIENT_ID = "4mt7pq78npi8s55lsetnq1g5c7";
+  const COGNITO_DOMAIN = "us-east-13k54foqzh.auth.us-east-1.amazoncognito.com";
+  const REDIRECT_URI = "https://taptupo.com/"; // redirect back to index after logout
+  const logoutUrl =
     `https://${COGNITO_DOMAIN}/logout?` +
     `client_id=${encodeURIComponent(CLIENT_ID)}` +
     `&logout_uri=${encodeURIComponent(REDIRECT_URI)}`;
-
-  // 3) Redirect browser to kill the Cognito session
+  // Redirect browser to kill the Cognito session
   window.location.replace(logoutUrl);
-
-  // 4) In case replace() is blocked, update the link
+  // In case replace() is blocked, update the link
   document.getElementById("continue-link").href = logoutUrl;
 })();
